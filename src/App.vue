15ed96b57e14233762bf8048277d343836e24973
costpawa <hakan_dnz_000@hotmail.com>
2021-09-21 16:50:17 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,146 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="app-header">
+      <div class="app-logo">
+        <img src="https://aws-amplify.github.io/images/Logos/Amplify-Logo-White.svg" alt="AWS Amplify">
+      </div>
+      <h1>Welcome to the Amplify Framework</h1>
     </div>
-    <router-view/>
+    <div class="app-body">
+      <amplify-authenticator>
+        <div class="welcome">
+          <h1>Hey, {{ user.username }}!</h1>
+          <amplify-sign-out></amplify-sign-out>
+        </div>
+      </amplify-authenticator>
+      <div v-if="user">
+        <div class="form-body">
+          <form v-on:submit.prevent autocomplete="off">
+            <div class="input">
+              <input type="text" class="message-input" v-model="form.message" autocomplete="off" placeholder="Enter your message" />
+            </div>
+            <button @click="sendMessage">Send</button>
+            <button @click="deleteAll">Delete All</button>
+          </form>
+        </div>
+        <div>
+          <div v-if="loading" class="loading">Loading...</div>
+          <div v-if="!loading" class="chatty-container">
+            <div class="chatty">
+              <div v-for="message of sorted" :key="message.id">
+                <div v-bind:class="[(user.username === message.user) ? 'me' : 'others']">
+                  <div class="message-container">
+                    <div class="message-user">
+                      {{ message.user }}
+                      <span
+                        class="message-time"
+                        :title="$options.filters.formatDateTime(message.createdAt)"
+                      >
+                        {{ message.createdAt | formatTime }}
+                      </span>
+                    </div>
+                    <div class="message-text">{{ message.message }}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
+import { DataStore, Predicates } from '@aws-amplify/datastore'
+import { Chatty } from './models'
+import moment from 'moment'
+import Vue from 'vue'
 
-#nav {
-  padding: 30px;
-}
+Vue.filter('formatTime', (value) => {
+  if(!value) return;
+  return moment(String(value)).format('HH:mm:ss')
+})
+Vue.filter('formatDateTime', (value) => {
+  if(!value) return;
+  return moment(String(value)).format('YYYY-MM-DD HH:mm:ss')
+})
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default ({
+  name: 'App',
+  data: () => ({
+    user: {},
+    form: {},
+    messages: [],
+    loading: false,
+  }),
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  created() {
+    this.subscription = DataStore.observe(Chatty).subscribe(msg => {
+      console.log(`${msg.model.name} - ${msg.opType}`, msg.element)
+      this.loadMessages()
+    })
+
+    onAuthUIStateChange((state, user) => {
+      switch(state) {
+        case AuthState.SignedIn: {
+          this.user = user
+          console.log(user)
+          this.loadMessages()
+          break
+        }
+      }
+    })
+  },
+
+  computed: {
+    sorted() {
+      let messages = [...this.messages]
+      return messages.sort((a, b) => -a.createdAt.localeCompare(b.createdAt))
+    }
+  },
+
+  methods: {
+    moment: () => moment(),
+    loadMessages() {
+      try {
+        this.loading = true
+        DataStore.query(Chatty, Predicates.ALL).then(messages => {
+          this.loading = false
+          this.messages = messages
+        })
+      } catch(err) {
+        console.log('Error loading messages', err)
+      } finally {
+        this.loading = false
+      }
+    },
+    sendMessage() {
+      const { message } = this.form
+      if(!message) return;
+      DataStore.save(
+        new Chatty({
+          user: this.user.username,
+          message: message,
+          createdAt: new Date().toISOString(),
+        })
+      )
+      .then(() => {
+        console.log('message created')
+        this.form = { message: '' }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
+    async deleteAll() {
+      await DataStore.delete(Chatty, Predicates.ALL)
+      .then(() => this.loadMessages())
+      .catch((err) => {
+        console.log('Error deleting all messages', err)
+      })
+    }
+  },
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+import awsconfig from './aws-exports'
+import './assets/css/styles.css'
+
+Amplify.configure(awsconfig)
 
 Vue.config.productionTip = false
 
